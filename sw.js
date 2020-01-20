5167f33d277307458ef28e9376dbea05c1316e8b
bengfarrell <E87bjZ4IgpkO>
2020-01-19 17:58:03 -0800

    Updates

---
+++ sw.js
@@ -0,0 +1,55 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [
+  {
+    "url": "app-f3d3fbbf.js",
+    "revision": "f658a5b28d9d23c09c451bd98598fabf"
+  },
+  {
+    "url": "focus-visible-7f07070e.js",
+    "revision": "d8b35c8e18152344cb11e9cf84cd4c64"
+  },
+  {
+    "url": "index.html",
+    "revision": "07f1c66e885a48bf1b570f2b4a9fe2d6"
+  },
+  {
+    "url": "polyfills/custom-elements-es5-adapter.84b300ee818dce8b351c7cc7c100bcf7.js",
+    "revision": "cff507bc95ad1d6bf1a415cc9c8852b0"
+  },
+  {
+    "url": "polyfills/dynamic-import.b745cfc9384367cc18b42bbef2bbdcd9.js",
+    "revision": "ed55766050be285197b8f511eacedb62"
+  },
+  {
+    "url": "polyfills/webcomponents.dae9f79d9d6992b6582e204c3dd953d3.js",
+    "revision": "fe4a22f36087db029cd3f476a1935410"
+  }
+].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"));
